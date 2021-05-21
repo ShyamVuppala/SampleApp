b1f65b18ddea52e8fd68b3dc2ea05644e490e5d9
Shyam Vuppala <shyam.vuppala@paysafe.com>
2021-05-21 13:07:48 +0530

    Added library as subtree and modifying lib

--- app/src/main/java/com/shyam/sampleapp/MainActivity.kt
+++ app/src/main/java/com/shyam/sampleapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.shyam.sampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shyam.samplelib.SampleUtil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        SampleUtil.getSampleText();
     }
 }
